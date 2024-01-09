29502d98a8f6e20faf0ef1b44dfa55df422c357d
neshwa.parveen <neshwa.parveen@chillarcards.com>
2024-01-09 15:13:02 +0530

    grid in swiftui

---
+++ Apple Framework/Framework.swift
@@ -0,0 +1,33 @@
+//
+//  Framework.swift
+//  Apple Framework
+//
+//  Created by Neshwa on 30/11/23.
+//
+
+import Foundation
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
+
+struct MockData {
+    
+    static let frameworks = [
+    
+        Framework(name: "top Clip", imageName: "app-lip", urlString: "https://developer.apple.com/app-clips", description: "I’ll be providing all the material you need to follow along, so all you need to do is show up ready to learn. I’ve even provided a “Tweet” button at the end of every day, which composes a tweet for you saying you completed the day, and includes the hashtag to help others find you, a celebratory graphic, and a link for others to find out more – use it! I’ve tried to make sure it takes less than one hour to complete each day, but there is a little natural variation. For example, day 4 is a little shorter because it has less ground to cover, and day 13 is a little longer because it has a few complex topics inside."),
+        
+        Framework(name: "top Clip", imageName: "app-lip", urlString: "https://developer.apple.com/app-clips", description: "I’ll be providing all the material you need to follow along, so all you need to do is show up ready to learn. I’ve even provided a “Tweet” button at the end of every day, which composes a tweet for you saying you completed the day, and includes the hashtag to help others find you, a celebratory graphic, and a link for others to find out more – use it! I’ve tried to make sure it takes less than one hour to complete each day, but there is a little natural variation. For example, day 4 is a little shorter because it has less ground to cover, and day 13 is a little longer because it has a few complex topics inside."),
+        
+        Framework(name: "top Clip", imageName: "app-lip", urlString: "https://developer.apple.com/app-clips", description: "I’ll be providing all the material you need to follow along, so all you need to do is show up ready to learn. I’ve even provided a “Tweet” button at the end of every day, which composes a tweet for you saying you completed the day, and includes the hashtag to help others find you, a celebratory graphic, and a link for others to find out more – use it! I’ve tried to make sure it takes less than one hour to complete each day, but there is a little natural variation. For example, day 4 is a little shorter because it has less ground to cover, and day 13 is a little longer because it has a few complex topics inside."),
+        
+        Framework(name: "top Clip", imageName: "app-lip", urlString: "https://developer.apple.com/app-clips", description: "I’ll be providing all the material you need to follow along, so all you need to do is show up ready to learn. I’ve even provided a “Tweet” button at the end of every day, which composes a tweet for you saying you completed the day, and includes the hashtag to help others find you, a celebratory graphic, and a link for others to find out more – use it! I’ve tried to make sure it takes less than one hour to complete each day, but there is a little natural variation. For example, day 4 is a little shorter because it has less ground to cover, and day 13 is a little longer because it has a few complex topics inside."),
+        
+        Framework(name: "top Clip", imageName: "app-lip", urlString: "https://developer.apple.com/app-clips", description: "I’ll be providing all the material you need to follow along, so all you need to do is show up ready to learn. I’ve even provided a “Tweet” button at the end of every day, which composes a tweet for you saying you completed the day, and includes the hashtag to help others find you, a celebratory graphic, and a link for others to find out more – use it! I’ve tried to make sure it takes less than one hour to complete each day, but there is a little natural variation. For example, day 4 is a little shorter because it has less ground to cover, and day 13 is a little longer because it has a few complex topics inside.")
+    
+    ]
+    
+}
